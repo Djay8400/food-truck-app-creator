a6230b50bc6a5b516d1dedf58a26f22c9621e4de
Denis Arce <dnsarce21@gmail.com>
2022-01-08 12:17:29 -0600

    added bp files for necesary technology for app development

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import foodTruck from "./assets/foodtruck.jpg";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <header className="App-header">
+        <img src={foodTruck} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import FoodItem from "../components/FoodItem";
+import Nav from "../components/Nav";
+// import Cart from "../components/Cart";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Nav />
+      <h1>FOOD TRUCK APP</h1>
+    </div>
+  );
+};
+
+export default Home;
