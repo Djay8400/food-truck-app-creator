eb51de8d29a36635634fd685317bcd5e152ccb7d
Denis Arce <dnsarce21@gmail.com>
2022-01-08 13:10:47 -0600

    updated code to redeploy in heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/food-truck-app",
+  process.env.MONGODB_URI || "mongodb://localhost/food-truck-fix",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
