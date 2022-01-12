8e0cf7d9bb8d6e0b32b5f5fc35f2e6d8c976aea6
Denis Arce <dnsarce21@gmail.com>
2022-01-12 15:11:52 -0600

    updated code

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,62 +1,114 @@
 import React from "react";
 import burgerBtn from "../js/burgerButton";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return (
-    <nav
-      className="navbar is-light"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io"></a>
+  <nav
+    className="navbar is-light"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="navbar-brand">
+      <a className="navbar-item" href="https://bulma.io"></a>
 
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          onClick={burgerBtn}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+      <a
+        role="button"
+        className="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        onClick={burgerBtn}
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div className="navbar-start">
-          <a class="navbar-item">Home</a>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div className="navbar-start">
+        <a class="navbar-item">Home</a>
 
-          <a className="navbar-item">Documentation</a>
+        <a className="navbar-item">Documentation</a>
 
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">More</a>
 
-            <div className="navbar-dropdown">
-              <a className="navbar-item">Profile</a>
-              <a className="navbar-item">Food-Trucks</a>
+          <div className="navbar-dropdown">
+            <a className="navbar-item">Profile</a>
+            <a className="navbar-item">Food-Trucks</a>
 
-              <hr className="navbar-divider" />
-              <a className="navbar-item">Report an issue</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-dark">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
-            </div>
+            <hr className="navbar-divider" />
+            <a className="navbar-item">Report an issue</a>
           </div>
         </div>
       </div>
-    </nav>
-  );
+
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a className="button is-dark">
+              <strong>Sign up</strong>
+            </a>
+            <a className="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>;
 }
 
 export default Nav;
+
+{
+  /* <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="https://bulma.io"></a>
+
+    <a
+      role="button"
+      className="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarBasicExample"
+      onClick={burgerBtn}
+    >
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div className="navbar-start">
+      <a class="navbar-item">Home</a>
+
+      <a className="navbar-item">Documentation</a>
+
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">More</a>
+
+        <div className="navbar-dropdown">
+          <a className="navbar-item">Profile</a>
+          <a className="navbar-item">Food-Trucks</a>
+
+          <hr className="navbar-divider" />
+          <a className="navbar-item">Report an issue</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a className="button is-dark">
+            <strong>Sign up</strong>
+          </a>
+          <a className="button is-light">Log in</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>; */
+}
