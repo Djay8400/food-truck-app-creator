affff5d89c7219857d1126b8486072d5818486e5
Denis Arce <dnsarce21@gmail.com>
2022-01-12 16:46:31 -0600

    updated app.js and nav.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,55 @@
 // import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 import "./App.css";
-import foodTruck from "./assets/foodtruck.jpg";
+// import foodTruck from "./assets/foodtruck.jpg";
 import Home from "./pages/Home";
+import { setContext } from "@apollo/client/link/context";
+// import { StoreProvider } from "./utils/GlobalState";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <img src={foodTruck} className="truck" alt="logo" />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          {/* <StoreProvider> */}
+          <Nav />
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Routes>
+          {/* </StoreProvider> */}
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
