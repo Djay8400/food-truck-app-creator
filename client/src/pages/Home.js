682c10ba612cea08e2f6786462911f89487e6621
David Jay <djay8400@gmail.com>
2022-01-13 23:33:01 -0600

    added dummy items to home page to test cart

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 // import FormCustomization from "../components/Form";
 // import FoodItem from "../components/FoodItem";
 import Nav from "../components/Nav";
+import ProductList from "../components/ProductList";
 import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="main-container">
       <h1>FOOD TRUCK APP</h1>
+      <ProductList />
       <Cart />
     </div>
   );
