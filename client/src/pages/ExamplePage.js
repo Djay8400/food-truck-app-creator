b1c6759581e08aa60085c11dc603facb83024439
David Jay <djay8400@gmail.com>
2022-01-18 15:18:19 -0600

    added logo placeholder to example page

--- client/src/pages/ExamplePage.js
+++ client/src/pages/ExamplePage.js
@@ -2,10 +2,20 @@ import React from "react";
 import Cart from "../components/Cart";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import yourLogoHere from "../assets/your-logo-here.png";
 
 const ExamplePage = () => {
   return (
     <div className="container">
+      <div className="hero-head">
+        <a className="" href="../">
+          <img
+            src={yourLogoHere}
+            alt="your-logo-here logo"
+            width={"300px"}
+          ></img>
+        </a>
+      </div>
       <CategoryMenu />
       <ProductList />
       <Cart />
