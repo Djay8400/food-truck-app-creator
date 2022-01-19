87a025d5c8e9feec9e537ef8c55955af30e0f700
Samuel Adams <samueladams412@gmail.com>
2022-01-19 16:43:20 -0600

    fixing secret key

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-require("dotenv").config();
-const secret = process.env.MY_SECRET;
+// require("dotenv").config();
+const secret = "mysecretsshhhhh";
 const expiration = '2h';
 
 module.exports = {
